f3832ba04e369ac66019b5bb74f92d2b358ec27b
Brian Thayer <bthayer1990@gmail.com>
2016-08-30 16:04:09 -0500

    completed static version of pizza creator; added bootstrap; moved bower_components

--- UI/public/scripts/newOrderItem.js
+++ UI/public/scripts/newOrderItem.js
@@ -1,18 +1,199 @@
+var NewOrderItemSubmission = React.createClass({
+    render: function(){
+        return(
+            <input type="button" className="btn btn-success btn-lg" value="Add to Order" />
+        );
+    }
+});
+
+var NewOrderItemTotal = React.createClass({
+    render: function(){
+        return(
+            <label className="totalLabel">Total: ${this.props.total}</label>
+        );
+    }
+});
+
 var NewOrderItemFooter = React.createClass({
     render: function(){
         return(
-            <h1>Footer</h1>
+            <div className="form-inline">
+                <div className="form-group">
+                    <NewOrderItemTotal total='00.00' />
+                </div>
+                <div className="form-group inlineLeftMargin">
+                    <NewOrderItemSubmission />
+                </div>
+            </div>
+        );
+    }
+});
+
+var CheckboxOption = React.createClass({
+    render: function() {
+        return(
+            <label className="checkbox-inline">
+                <input type="checkbox" value={this.props.value} />{this.props.value}
+            </label>
+        );
+    }
+});
+
+var RadioOption = React.createClass({
+    render: function() {
+        return(
+            <label className="radio-inline">
+                <input type="radio" name={this.props.section} value={this.props.value} />{this.props.value}
+            </label>
+        );
+    }
+});
+
+var PizzaCreatorToppings = React.createClass({
+    render: function() {
+        var meatOptions = [];
+        var veggieOptions = [];
+        this.props.toppings.forEach(function(topping){
+            if(topping.type === 'meat'){
+                meatOptions.push(<CheckboxOption value={topping.value} />)
+            }
+            else{
+                veggieOptions.push(<CheckboxOption value={topping.value} />)
+            }
+        });
+        return(
+            <div>
+                <h3>Toppings</h3>
+                <br/>
+                <div className="optionsContainer">
+                    <h4>Meats</h4>
+                    {meatOptions}
+                    <br/><br/>
+                    <h4>Veggies</h4>
+                    {veggieOptions}
+                </div>
+            </div>
+        );
+    }
+});
+
+var PizzaCreatorCheese = React.createClass({
+    render: function() {
+        return(
+            <div>
+                <h3>Cheese</h3>
+                <div className="optionsContainer">
+                    <RadioOption section="cheese" value="yes" />
+                    <RadioOption section="cheese" value="no" />
+                </div>
+            </div>
+        );
+    }
+});
+
+var PizzaCreatorSauce = React.createClass({
+    render: function() {
+        var options = [];
+        this.props.sauces.forEach(function(sauce) {
+            options.push(<RadioOption section="sauce" value={sauce.value} />);    
+        });
+        return(
+            <div>
+                <h3>Sauce</h3>
+                <div className="optionsContainer">
+                    {options}
+                </div>
+            </div>
+        );
+    }
+});
+
+var PizzaCreatorCrust = React.createClass({
+    render: function() {
+        var options = [];
+        this.props.crusts.forEach(function(crust) {
+            options.push(<RadioOption section="crust" value={crust.value} />);    
+        });
+        return(
+            <div>
+                <h3>Crust</h3>
+                <div className="optionsContainer">
+                    {options}
+                </div>
+            </div>
+        );
+    }
+});
+
+
+
+var PizzaCreatorSize = React.createClass({
+    render: function() {
+        var options = [];
+        this.props.sizes.forEach(function(size) {
+            options.push(<RadioOption section="size" value={size.value} />);    
+        });
+        return(
+            <div>
+                <h3>Size</h3>
+                <div className="optionsContainer">
+                    {options}
+                </div>
+            </div>
         );
     }
 });
 
 var PizzaCreatorNewOrderItemContent = React.createClass({
     render: function() {
+        //TODO: get sizes, crusts, sauces, and toppings from API
         return(
-            <h1>Pizza Creator</h1>
+            <div>
+                <h1 className="text-center">Pizza Creator</h1>
+                <PizzaCreatorSize sizes={SIZES} /><hr/>
+                <PizzaCreatorCrust crusts={CRUSTS} /><hr/>
+                <PizzaCreatorSauce sauces={SAUCES} /><hr/>
+                <PizzaCreatorCheese /><hr/>
+                <PizzaCreatorToppings toppings={TOPPINGS} /><br/><hr/>
+            </div>
         );
     }
 });
+
+var SIZES = [
+    {value: 'small'},
+    {value: 'medium'},
+    {value: 'large'},
+    {value: 'extra large'}
+]
+
+var CRUSTS = [
+    {value: 'hand-tossed'},
+    {value: 'deep dish'},
+    {value: 'thin'}
+]
+
+var SAUCES = [
+    {value: 'marinara'},
+    {value: 'bbq'},
+    {value: 'ranch'},
+    {value: 'buffalo'}
+]
+
+var TOPPINGS = [
+    {type: 'meat', value: 'pepperoni'},
+    {type: 'meat', value: 'sausage'},
+    {type: 'meat', value: 'bacon'},
+    {type: 'meat', value: 'canadian bacon'},
+    {type: 'meat', value: 'hamburger'},
+    {type: 'meat', value: 'steak'},
+    {type: 'veggie', value: 'peppers'},
+    {type: 'veggie', value: 'onions'},
+    {type: 'veggie', value: 'olives'},
+    {type: 'veggie', value: 'spinach'},
+    {type: 'veggie', value: 'mushrooms'},
+    {type: 'veggie', value: 'garlic'}
+]
 
 var NewOrderItemContent = React.createClass({
     render: function() {
