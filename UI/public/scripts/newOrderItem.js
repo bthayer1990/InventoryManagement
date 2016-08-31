7d20bdf6a0fd56d0361c77e14a2887036be0b47d
MIR\bthaye01 <bthayer@wustl.edu>
2016-08-31 12:57:03 -0500

    completed size change event handler

--- UI/public/scripts/newOrderItem.js
+++ UI/public/scripts/newOrderItem.js
@@ -40,10 +40,15 @@ var CheckboxOption = React.createClass({
 });
 
 var RadioOption = React.createClass({
+    handleChange: function() {
+        this.props.onSelection(
+            this.refs.option.value
+        );
+    },
     render: function() {
         return(
             <label className="radio-inline">
-                <input type="radio" name={this.props.section} value={this.props.value} />{this.props.value}
+                <input type="radio" name={this.props.section} value={this.props.value} ref="option" checked={this.props.selected} onChange={this.handleChange} />{this.props.value}
             </label>
         );
     }
@@ -128,10 +133,19 @@ var PizzaCreatorCrust = React.createClass({
 
 
 var PizzaCreatorSize = React.createClass({
+    handleChange: function(selection) {
+        this.props.onSelection(selection);
+    },
     render: function() {
         var options = [];
+        var selectedSize = this.props.selectedSize;
+        var handleChangeEvent = this.handleChange;
         this.props.sizes.forEach(function(size) {
-            options.push(<RadioOption section="size" value={size.value} />);    
+            var selected = false;
+            if(selectedSize == size.value){
+                selected = true;
+            }
+            options.push(<RadioOption section="size" value={size.value} selected={selected} onSelection={handleChangeEvent} />);    
         });
         return(
             <div>
@@ -145,18 +159,52 @@ var PizzaCreatorSize = React.createClass({
 });
 
 var PizzaCreatorNewOrderItemContent = React.createClass({
+    //TODO: get sizes, crusts, sauces, and toppings from API
+    //TODO: post selected data
+    //TODO: calculate total based on selections
+    getInitialState: function() {
+        return {
+            selectedSize: '',
+            selectedCrust: '',
+            selectedSauce: '',
+            selectedCheese: '',
+            selectedToppings: []
+        };
+    },
+    handleSizeSelection: function(selectedSize) {
+        this.setState({
+            selectedSize: selectedSize
+        });
+    },
+    handleCrustSelection: function(selectedCrust) {
+        this.setState({
+            selectedCrust: selectedCrust
+        });
+    },
+    handleSauceSelection: function(selectedSauce) {
+        this.setState({
+            selectedSauce: selectedSauce
+        });
+    },
+    handleCheeseSelection: function(selectedCheese) {
+        this.setState({
+            selectedCheese: selectedCheese
+        });
+    },
+    handleToppingSelection: function(selectedToppings) {
+        this.setState({
+            selectedToppings: selectedToppings
+        });
+    },
     render: function() {
-        //TODO: get sizes, crusts, sauces, and toppings from API
-        //TODO: post selected data
-        //TODO: calculate total based on selections
         return(
             <div>
                 <h1 className="text-center">Pizza Creator</h1>
-                <PizzaCreatorSize sizes={SIZES} /><hr/>
-                <PizzaCreatorCrust crusts={CRUSTS} /><hr/>
-                <PizzaCreatorSauce sauces={SAUCES} /><hr/>
-                <PizzaCreatorCheese /><hr/>
-                <PizzaCreatorToppings toppings={TOPPINGS} /><br/><hr/>
+                <PizzaCreatorSize sizes={SIZES} selectedSize={this.state.selectedSize} onSelection={this.handleSizeSelection} /><hr/>
+                <PizzaCreatorCrust crusts={CRUSTS} selectedCrust={this.state.selectedCrust} onSelection={this.handleCrustSelection} /><hr/>
+                <PizzaCreatorSauce sauces={SAUCES} selectedSauce={this.state.selectedSauce} onSelection={this.handleSauceSelection} /><hr/>
+                <PizzaCreatorCheese selectedCheese={this.state.selectedCheese} onSelection={this.handleCheeseSelection} /><hr/>
+                <PizzaCreatorToppings toppings={TOPPINGS} selectedToppings={this.state.selectedToppings} onSelection={this.handleToppingSelection} /><br/><hr/>
                 <NewOrderItemFooter total="00.00"/>
             </div>
         );
