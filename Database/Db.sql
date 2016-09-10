0d6e6b0338abffadce3f79b30f4f47c565205f57
Brittan Edler-Warbin <edlerwb@gmail.com>
2016-09-10 10:14:17 -0500

    Add database sql, connect options.js to database

---
+++ Database/Db.sql
@@ -0,0 +1,104 @@
+/* these commands need to be run independently from the ones below them because you will need
+to connect to the newly created database in order to run the next set*/
+
+CREATE ROLE pizza LOGIN
+  PASSWORD 'pizzadb'
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+
+-- DROP DATABASE pizza;
+
+CREATE DATABASE pizza
+  WITH OWNER = pizza
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8'
+       CONNECTION LIMIT = -1;
+
+/* The commands below should be run after the top two commands once you've connected to the created database
+*/
+
+CREATE SCHEMA pizzaown
+  AUTHORIZATION pizza;
+
+  -- Tables
+
+CREATE TABLE pizzaown.category
+(
+  id serial NOT NULL,
+  name text,
+  CONSTRAINT "categoryPk" PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE pizzaown.category
+  OWNER TO pizza;
+
+CREATE TABLE pizzaown.price
+(
+  id serial NOT NULL,
+  price money,
+  CONSTRAINT "pricePk" PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE pizzaown.price
+  OWNER TO pizza;
+
+CREATE TABLE pizzaown.topping
+(
+  id serial NOT NULL,
+  name text,
+  CONSTRAINT "toppingPk" PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE pizzaown.topping
+  OWNER TO pizza;
+
+-- Table: pizzaown."toppingCategory"
+
+-- DROP TABLE pizzaown."toppingCategory";
+
+CREATE TABLE pizzaown."toppingCategory"
+(
+  "toppingId" integer NOT NULL,
+  "categoryId" integer,
+  CONSTRAINT "toppingCategoryPk" PRIMARY KEY ("toppingId"),
+  CONSTRAINT "categoryFk" FOREIGN KEY ("categoryId")
+      REFERENCES pizzaown.category (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT "toppingFk" FOREIGN KEY ("toppingId")
+      REFERENCES pizzaown.topping (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE pizzaown."toppingCategory"
+  OWNER TO pizza;
+
+-- Table: pizzaown."toppingPrice"
+
+-- DROP TABLE pizzaown."toppingPrice";
+
+CREATE TABLE pizzaown."toppingPrice"
+(
+  "toppingId" integer NOT NULL,
+  "priceId" integer,
+  CONSTRAINT "toppingPricePk" PRIMARY KEY ("toppingId"),
+  CONSTRAINT "priceFk" FOREIGN KEY ("priceId")
+      REFERENCES pizzaown.price (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT "toppingFk" FOREIGN KEY ("toppingId")
+      REFERENCES pizzaown.topping (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE pizzaown."toppingPrice"
+  OWNER TO pizza;
