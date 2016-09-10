ef6bf3140b6ee2a80eb3c9e010ab468d0dd0a9a0
Brittan Edler-Warbin <edlerwb@gmail.com>
2016-09-10 10:14:50 -0500

    connect options.js to database

--- Services/routes/options.js
+++ Services/routes/options.js
@@ -1,18 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var config = {
+  user: 'pizza',
+  database: 'pizza',
+  password: 'pizzadb',
+  port: 5432, 
+  max: 10,
+  idleTimeoutMillis: 30000,
+};
+
+var pool = new pg.Pool(config);
 
 var sizeData = [
     {size: 'Small', price: 12.00},
     {size: 'Medium', price: 14.00},
     {size: 'Large', price: 16.00},
     {size: 'Extra-Large', price: 18.00}
-];
-var toppingData = [
-    {name: 'Pepperoni', category: 'Meat', price: 1.50},
-    {name: 'Sausage', category: 'Meat', price: 1.50},
-    {name: 'Cheese', category: 'Cheese', price: 1.00},
-    {name: 'Black Olives', category: 'Vegetable', price: 1.00},
-    {name: 'Green Peppers', category: 'Vegetable', price: 1.00}
 ];
 var sauceData = [
     {name: 'Red'},
@@ -31,7 +35,24 @@ router.get('/sizes', function(req, res, next) {
 });
 
 router.get('/toppings', function(req, res, next) {
-    res.send({toppings: toppingData});
+    pool.connect(function(err, client, done) {
+        if(err) {
+            res.err('error fetching client from pool', err);
+        }
+        var results = [];
+        client.query('select t."name", c."name", p."price" ' +
+            'from pizzaown."topping" t ' +
+            'left join pizzaown."toppingCategory" tc on tc."toppingId" = t."id" ' +
+            'left join pizzaown."category" c on c."id" = "categoryId" ' +
+            'left join pizzaown."toppingPrice" tp on tp."toppingId" = t."id" ' +
+            'left join pizzaown."price" p on p."id" = tp."priceId"', function(err, result) {
+                result.rows.forEach(function(item) {
+                    results.push(item);
+                });
+                res.send({toppings: results});
+            });
+        done();
+    });
 });
 
 router.get('/sauces', function(req, res, next) {
